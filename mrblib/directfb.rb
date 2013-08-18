71cc78c58eaacde87327f29aaea8514b193703d3
uwitty <uyuwitty@gmail.com>
2013-08-18 10:19:29 +0900

    improve mrblib/directfb.rb

--- mrblib/directfb.rb
+++ mrblib/directfb.rb
@@ -6,25 +6,50 @@ class DirectFB
       @initialized = true
     end
 
-    dfb = DirectFB.create()
-    layer = dfb.get_display_layer(DLID_PRIMARY)
-    layer.set_cooperative_level(DLSCL_ADMINISTRATIVE)
-    layer.set_configuration(layer_conf) if layer_conf
-    primary = layer.get_surface()
+    begin
+      dfb = DirectFB.create()
+      layer = dfb.get_display_layer(DLID_PRIMARY)
+      layer.set_cooperative_level(DLSCL_ADMINISTRATIVE)
+      layer.set_configuration(layer_conf) if layer_conf
+      primary = layer.get_surface()
 
-    yield dfb, layer, primary
+      yield dfb, layer, primary
+    ensure
+      primary.release if primary
+      layer.release if layer
+      dfb.release if dfb
+    end
+  end
 
-    primary.release if primary
-    layer.release if layer
-    dfb.release if dfb
+  def self.run2(layer_conf=nil)
+    unless @initialized
+      ret = DirectFB.init()
+      DirectFB.error('DirectFB.init()', ret)
+      @initialized = true
+    end
+
+    begin
+      dfb = DirectFB.create()
+      layer = dfb.get_display_layer(DLID_PRIMARY)
+      layer.set_cooperative_level(DLSCL_ADMINISTRATIVE)
+      layer.set_configuration(layer_conf) if layer_conf
+
+      yield dfb, layer
+    ensure
+      layer.release if layer
+      dfb.release if dfb
+    end
   end
 
   class Surface
     def lock(flags)
       if block_given?
         ptr, pitch = self.lock_impl flags
-        yield ptr, pitch
-        self.unlock if ptr
+        begin
+          yield ptr, pitch
+        ensure
+          self.unlock if ptr
+        end
       else
         self.lock_impl flags
       end
